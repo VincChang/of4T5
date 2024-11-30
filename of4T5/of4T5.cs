6fbccceeb34d55e763676145e5a7111b41bfe527
Vincent Chang <vincent_chang137@hotmail.com>
2024-11-30 11:52:19 +0800

    Bounce function for Fortify

---
+++ of4T5/of4T5.cs
@@ -0,0 +1,37 @@
+﻿namespace System
+{
+    /// <summary>
+    /// echo return
+    /// </summary>
+    public class of4T5
+    {
+        public static string Bounce(string text)
+        {
+            return text;
+        }
+        public static int Bounce(int value)
+        {
+            return value;
+        }
+        public static float Bounce(float value)
+        {
+            return value;
+        }
+        public static decimal Bounce(decimal value)
+        {
+            return value;
+        }
+        public static Int64 Bounce(Int64 value)
+        {
+            return value;
+        }
+        public static T Bounce<T>(T value) where T : class
+        {
+            return value;
+        }
+        public static List<T> Bounce<T>(List<T> value) where T : class
+        {
+            return value;
+        }
+    }
+}
